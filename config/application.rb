9f4de10cb03d38579b282f9bea7a2c55189b66ae
sugawara <pgjsugawara258@icloud.com>
2018-09-05 15:54:00 +0900

    レイアウトの初期設定完了

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module RailsSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.skip_routes true
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
