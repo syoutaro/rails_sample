6eabd5983ffa4123384f0c035e0514b80364af7d
sugawara <pgjsugawara258@icloud.com>
2018-09-06 20:21:49 +0900

    ユーザーネームの追加完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,51 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  confirmation_token     :string
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string
 #  failed_attempts        :integer          default(0), not null
 #  unlock_token           :string
 #  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string
 #
 
 class User < ApplicationRecord
+  validates :name,
+          presence: :true,
+          uniqueness: { case_sensitive: false }
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable
+         :lockable, :timeoutable
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+     where(conditions.to_h).where(["lower(name) = :value OR lower(email) = :value",
+      { :value => login.downcase }]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+     where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: name).exists?
+      errors.add(:name, :invalid)
+    end
+  end
+
 end
